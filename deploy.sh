e05f34ffdc636767bddf75c769ac4a3c19af7c3f
Pratham Onkar Singh <prathamaj09@gmail.com>
2026-02-09 00:00:20 +0530

    building on github actions and using ec2 just for running because t3.micro cant handle running typescript

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "Starting deployment..."
+echo "Restarting app..."
 
-# Load nvm
-export NVM_DIR="$HOME/.nvm"
-source "$NVM_DIR/nvm.sh"
-
-# Use Node 22
-nvm use 22
-
-echo "Node version:"
-node -v
-
-# Go to project directory
-cd ~/ci-cd-practice
-
-# Pull latest code and merge both states in ec2 and github
-git fetch origin main
-git reset --hard origin/main
-
-# Install dependencies and build
-npm install
-npm run build
-
-# Start app
 pm2 restart ci-cd-practice || pm2 start npm --name "ci-cd-practice" -- start
-
-echo "Deployment finished"
